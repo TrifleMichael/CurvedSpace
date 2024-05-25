924ada186b2ff0c265a6b7a69c7b79d0f998c78e
MichalT <trzebuniak.michal@gmail.com>
2024-05-25 13:32:43 +0200

    Alpha channel fix

--- main/src/app/SimulationHandler.java
+++ main/src/app/SimulationHandler.java
@@ -182,6 +182,8 @@ public class SimulationHandler {
 
         // Set the clear color
         glClearColor(0.0f, 0.0f, 0.1f, 0.0f);
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
         // Run the rendering loop until the user has attempted to close
         // the window or has pressed the ESCAPE key.
@@ -203,7 +205,7 @@ public class SimulationHandler {
             simulatePhysics();
             drawFrame();
             if (inMainMenu) {
-                drawTexture(logoTexture, 0, Settings.windowX, Settings.windowY - 300, Settings.windowY);
+                TextureDrawer.drawTexture(logoTexture, 0, Settings.windowX, Settings.windowY - 300, Settings.windowY);
             }
 
             glfwSwapBuffers(window); // swap the color buffers
@@ -217,65 +219,7 @@ public class SimulationHandler {
     }
 
     private void loadTextures() {
-        logoTexture = loadTexture("E:\\Repos\\CurvedSpace\\main\\src\\textures\\logo.png");
+        logoTexture = TextureDrawer.loadTexture("E:\\Repos\\CurvedSpace\\main\\src\\textures\\logo.png");
     }
 
-    private void drawTexture(int textureId, int xl, int xr, int yd, int yu) {
-        glBindTexture(GL_TEXTURE_2D, textureId);
-
-        // Render the textured quad
-        glEnable(GL_TEXTURE_2D);
-        glBegin(GL_QUADS);
-        glTexCoord2f(0, 0);
-        glVertex2f(xl, yu);
-        glTexCoord2f(1, 0);
-        glVertex2f(xr, yu);
-        glTexCoord2f(1, 1);
-        glVertex2f(xr, yd);
-        glTexCoord2f(0, 1);
-        glVertex2f(xl, yd);
-        glEnd();
-        glDisable(GL_TEXTURE_2D);
-
-    }
-
-    private int loadTexture(String path) {
-        int width, height;
-        ByteBuffer image;
-        try (MemoryStack stack = MemoryStack.stackPush()) {
-            IntBuffer w = stack.mallocInt(1);
-            IntBuffer h = stack.mallocInt(1);
-            IntBuffer comp = stack.mallocInt(1);
-
-            // Load the image
-            image = STBImage.stbi_load(path, w, h, comp, 4);
-            if (image == null) {
-                throw new RuntimeException("Failed to load image: " + STBImage.stbi_failure_reason());
-            }
-
-            width = w.get();
-            height = h.get();
-        }
-
-        // Create a new OpenGL texture
-        int textureId = glGenTextures();
-        glBindTexture(GL_TEXTURE_2D, textureId);
-
-        // Set the texture parameters
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-        // Upload the texture data
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image);
-
-        // Generate mipmaps
-        glGenerateMipmap(GL_TEXTURE_2D); // TODO is this the good import?
-
-        // Free the image memory
-        STBImage.stbi_image_free(image);
-
-        return textureId;
-    }
 }
